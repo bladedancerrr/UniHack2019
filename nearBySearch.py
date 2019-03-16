f30e36cb2fd69e0a4060ce331a2dc0809163134f
jieyan <jyan1907@gmail.com>
2019-03-17 07:46:34 +1100

    calculates string match percentage

--- nearBySearch.py
+++ nearBySearch.py
@@ -29,7 +29,24 @@ def find_nearby_buildings():
 		building_results.append(temp)
 		print(temp)
 
+def string_match_percentage(str1, str2):
+	str1 = str1.lower().split()
+	str2 = str2.lower().split()
+
+	count = 0
+
+	if len(str1) < len(str2):
+		for word in str1:
+			if word in str2:
+				count += 1
+		return count / len(str2)
+	else:
+		for word in str2:
+			if word in str1:
+				count += 1
+		return count / len(str1)
 
 
 if __name__ == "__main__":
 	find_nearby_buildings()
+	print(string_match_percentage("Hello There", "hello"))
